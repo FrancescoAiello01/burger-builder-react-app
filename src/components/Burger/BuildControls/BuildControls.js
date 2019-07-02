490830d3795e2a2222a1ecc30d0020c7301007fc
Francesco Aiello <faiello@kollective.com>
2019-07-02 13:32:52 -0700

    burger builder finished and price displayed

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,13 +10,16 @@ const controls = [
   { label: "Meat", type: "meat" }
 ];
 
-const buildControls = props => (
+const buildControls = props => ( // Rounds price to 2 decimal places
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p> 
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
